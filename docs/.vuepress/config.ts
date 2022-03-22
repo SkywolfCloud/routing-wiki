558cbb8fb9cce1902799683944792f40f47569d2
pppwaw <pppwaw@pppwaw.top>
2022-03-22 19:35:01 +0800

    分为详细与快速开始两部分

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,21 +2,22 @@ module.exports = {
     title: 'Bird Wiki',
     description: '一篇关于bird的百科（或者手册）',
     themeConfig: {
-        sidebar: [
-            '/介绍/README.md',
+        sidebar: {"/specific/": [
+
+            '/specific/介绍/README.md',
             {
                 text: "配置",
-                link: "/配置/",
+                link: "/specific/配置/",
                 children: [
-                    '/配置/README.md',
-                    '/配置/Global.md',
-                    '/配置/Protocol.md',
-                    '/配置/Channel.md',
-                    '/配置/Template.md'
+                    '/specific/配置/README.md',
+                    '/specific/配置/Global.md',
+                    '/specific/配置/Protocol.md',
+                    '/specific/配置/Channel.md',
+                    '/specific/配置/Template.md'
                 ]
             },
-            '/BGP/',
-            '/过滤器/'
-        ]
+            '/specific/BGP/',
+            '/specific/过滤器/'
+        ]}
     },
 }
--- docs/README.md
+++ docs/README.md
@@ -2,16 +2,10 @@
 lang: zh-CN
 home: true
 actions:
-  - text: 开始阅读
-    link: /介绍/
+  - text: 快速开始
+    link: /quickstart/
+  - text: 详细wiki
+    link: /specific/介绍/
 ---
 
-目前进度：
-
-| 板块  | 完成情况(✅❌) |
-|:----|----------|
-| 介绍  | ✅        |
-| BGP | ❌        |
-| 过滤器 | ❌        |
-| 配置  | ❌        |
 
