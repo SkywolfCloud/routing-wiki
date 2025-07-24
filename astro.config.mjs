62ba45440983d97ed62272cb853c134bb113b881
pppwaw <pppwaw@pppwaw.top>
2025-07-24 23:44:06 +0800

    Rewrite (#3)
    重写

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,95 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import fs from "fs";
+import mermaid from "astro-mermaid";
+import rehypeExternalLinks from "rehype-external-links";
+import remarkMath from "remark-math";
+import rehypeMathjax from "rehype-mathjax";
+const birdLang = JSON.parse(
+  fs.readFileSync("./langs/bird2.tmLanguage.json", "utf8")
+);
+const interfacesLang = JSON.parse(
+  fs.readFileSync("./langs/interfaces.tmLanguage.json", "utf8")
+);
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Routing Wiki',
-			description: '一篇关于路由的百科（或者手册）',
-			social: [
-				{ 
-					icon: 'github', 
-					label: 'GitHub', 
-					href: 'https://github.com/SkywolfCloud/routing-wiki' 
-				}
-			],
-			sidebar: [
-				{
-					label: '快速开始',
-					items: [
-						{ label: '概述', slug: 'quickstart' },
-						{ label: '准备工作', slug: 'quickstart/before' },
-						{ label: '配置客户端', slug: 'quickstart/player' },
-						{ label: 'IDC配置', slug: 'quickstart/idc' },
-						{ label: '多地部署', slug: 'quickstart/multi-location' },
-						{ label: '后续工作', slug: 'quickstart/after' },
-					],
-				},
-				{
-					label: 'Bird Wiki',
-					link: 'https://bird.xmsl.dev/',
-				},
-			],
-		}),
-	],
+  integrations: [
+    mermaid({
+      autoTheme: true,
+    }),
+    starlight({
+      title: "Routing Wiki",
+      description: "一篇关于路由的百科（或者手册）",
+      tableOfContents: { minHeadingLevel: 1, maxHeadingLevel: 3 },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/SkywolfCloud/routing-wiki",
+        },
+      ],
+      sidebar: [
+        {
+          label: "新手教程",
+          items: [
+            "beginner",
+            "beginner/before",
+            "beginner/bring-up-a-bgp-session",
+            {
+              label: "三、与他人连接",
+              items: [
+                "beginner/connect-with-others/introduction",
+                "beginner/connect-with-others/concept",
+                "beginner/connect-with-others/filters",
+                "beginner/connect-with-others/lab",
+                "beginner/connect-with-others/role",
+              ],
+            },
+            {
+              label: "四、多节点部署",
+              items: [
+                "beginner/multi-location/introduction",
+                "beginner/multi-location/igp",
+                "beginner/multi-location/ibgp",
+                "beginner/multi-location/lab",
+              ],
+            },
+            "beginner/after",
+            "beginner/troubleshooting",
+            "beginner/bring-home",
+          ],
+        },
+        {
+          label: "杂项",
+          items: ["misc","misc/tools"],
+        },
+        {
+          label: "Bird Wiki",
+          link: "https://bird.xmsl.dev/",
+        },
+      ],
+      expressiveCode: {
+        shiki: {
+          langs: [birdLang, interfacesLang],
+        },
+        defaultProps: {
+          wrap: true,
+        },
+      },
+    }),
+  ],
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          rel: ["noopener", "noreferrer"],
+          target: "_blank",
+        },
+      ],
+      rehypeMathjax,
+    ],
+    remarkPlugins: [remarkMath],
+  },
 });
